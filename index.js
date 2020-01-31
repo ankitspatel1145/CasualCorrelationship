e3be9f400e6f10e0891b35394056fd954cbe4167
kit <ankitspatel1145@gmail.com>
2020-01-31 14:18:46 -0800

    add all files

---
+++ index.js
@@ -0,0 +1,66 @@
+
+
+const output = fetch("https://api.cryptonator.com/api/ticker/btc-usd")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    // Work with JSON data here
+
+    console.log(data);
+  })
+  .then(data => {
+    renderChart()
+  })
+  .catch(err => {
+    // Do something for an error here
+  });
+//
+
+const renderChart = () => {
+
+  var ctx = document.getElementById("myChart")
+  
+  
+  var myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 13, 5, 12, 3],
+          backgroundColor: [
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)"
+          ],
+          borderColor: [
+            "rgba(255, 206, 86, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 99, 132, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)"
+          ],
+          borderWidth: 2
+        }
+      ]
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  });
+}
+
