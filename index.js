9e22a170a21da7e8e9a5500a79beb6c525dd223e
kit <ankitspatel1145@gmail.com>
2020-02-10 16:26:56 -0800

    display 2 datasets on graph

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 window.onload = function() {
-  // let exchangeData = allExchange()
-  let data1 = buildData()
-  var chart = BuildChart(data1);
+  getAllData()
 }
 
-// console.log(test1)
+const getAllData = async()=> {
+  let dataSet1 = await buildExchangeData()
+  // let dataSet2 = await buildExchangeData()
+  dataSet2 = {data:[1,4,6,3,4],
+              title: "testtitle"}
+  // console.log(stuff1)
+  BuildChart(dataSet1, dataSet2)
+}
 
-const buildData = () => {
-  let test1 = []
+const buildExchangeData = async () => {
+  let money = regions[Math.floor(Math.random() * regions.length)];
+  let data = [];
+
   i = 0
   while (i < 5){
-    exchangeAPI(i, test1)
+    await exchangeAPI(i, data, money)
     i = i + 1
   }
-  console.log("in buildData", test1)
+  output = {data,
+            title: `EUR to ${money} exchange rate`
+  }
+  return output
 }
 
 
-function BuildChart(input1) {
-  // console.log("build:", test1)
+function BuildChart(dataSet1, dataSet2) {
+  console.log("chart:", dataSet1.data)
+  console.log("chart:", dataSet2.data)
   var ctx = document.getElementById("myChart").getContext("2d");
   var myChart = new Chart(ctx, {
     type: "line",
@@ -26,19 +37,19 @@ function BuildChart(input1) {
       labels: ["2016", "2017", "2018", "2019", "2020"],
       datasets: [
         {
-          label: "test",
-          data: input1,
+          label: dataSet1.title,
+          data: dataSet1.data,
           backgroundColor: ["rgba(255, 99, 132, 0.2)"],
           borderColor: ["rgba(255, 99, 132, 1)"],
           borderWidth: 1
+        },
+        {
+          label: dataSet2.title,
+          data: dataSet2.data,
+          backgroundColor: ["rgba(54, 162, 235, 0.2)"],
+          borderColor: ["rgba(54, 162, 235, 1)"],
+          borderWidth: 1
         }
-        // {
-        //   label: "test 2",
-        //   data: [],
-        //   backgroundColor: ["rgba(54, 162, 235, 0.2)"],
-        //   borderColor: ["rgba(54, 162, 235, 1)"],
-        //   borderWidth: 1
-        // }
       ]
     },
     options: {
@@ -108,42 +119,28 @@ const regions = [
   "HRK"
 ]
 
-const exchangeAPI = (i, arr) => {
-  let money = regions[Math.floor(Math.random() * regions.length)];
+const exchangeAPI = async(i, output, money) => {
   let today = new Date();
   let date = today.getDate();
   let month = today.getMonth();
   let year = today.getFullYear();
-  // let i = 0;
-
-
+  
   let newDate = year - i + "-" + month + "-" + (date - 1);
   let url = `https://api.exchangeratesapi.io/${newDate}?symbols=${money}`
-  fetch(url)
-    .then(response => {
-      if (!response.ok ) {
-        location.reload();
-      }
-      return response.json()})
+  return fetch(url)
+    .then(response => response.json())
     .then(data => {
       let val = Object.values(data.rates)
-      arr.push(val[0])
-      // return val
-    })
-    .then(data => {
-      console.log("in exchange api", arr.length) 
-      return arr
+      output.push(val[0])
     })
     .catch(err => {
-    // location.reload();
+    location.reload();
     console.log("error:", err);
     });
-
-  // return test1
 };
 
 
-// exchangeAPI()  
+// StateAPI()  
 const states = [
   'Alabama',
   'Alaska',
